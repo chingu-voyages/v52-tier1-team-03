18848ce45e31894833aee1feeb17933638f45a20
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-12-12 12:58:28 -0500

    Add Firebase Hosting configuration

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'v52-tier1-team-03',
+  location: 'us-central1'
+};
+
